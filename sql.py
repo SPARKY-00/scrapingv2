989e34ef43f578e8a165792c5bc014ed059d96c2
Pgbgroup <80152512+Pgbgroup@users.noreply.github.com>
2022-04-23 16:33:50 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
-    engine = create_engine("postgres://ykfmxfem:Pm3VDsQXsWt8c0hbaPUYdM9bGNgtMPjl@fanny.db.elephantsql.com/ykfmxfem", client_encoding="utf8")
+    engine = create_engine("postgres://ilqctutl:qzwmPDdcS1LbTucOAw4lubaWurETMNSZ@john.db.elephantsql.com/ilqctutl", client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
